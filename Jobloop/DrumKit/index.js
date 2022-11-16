8d34484131bb52914a7326b19d7ccc10342a875f
Ahmad Mustafa <112854816+Ahmadkodehode@users.noreply.github.com>
2022-11-16 10:30:14 +0100

    non

--- Jobloop/DrumKit/index.js
+++ Jobloop/DrumKit/index.js
@@ -3,6 +3,13 @@ import { sounds } from "./data.js";
 const myDiv = document.getElementById("app");
 console.log(sounds);
 sounds.forEach((sound) => {
-  let y = document.createElement("button");
-  myDiv.append(y);
+  let myButton = document.createElement("button");
+  let y = sound.replace(".wav", "");
+  console.log(y);
+  myButton.textContent = sound.replace(".wav", "");
+  myDiv.append(myButton);
+  myButton.addEventListener("click", () => {
+    const audioElement = new Audio("./sounds/" + sound);
+    audioElement.play();
+  });
 });
